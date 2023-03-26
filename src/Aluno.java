80953a415d8e5ea1e87789742fa06224285a579b
RobinsonXavier <robinson.webzen@gmail.com>
2023-03-26 14:25:29 -0300

    added Aluno class, finished Aplicacao class

---
+++ src/Aluno.java
@@ -0,0 +1,33 @@
+public class Aluno {
+  private String nome;
+  private String email;
+  private String cpf;
+  private String senha;
+  
+  public String getNome() {
+    return nome;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public String getCpf() {
+    return cpf;
+  }
+  public String getSenha() {
+    return senha;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  
+}
--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,3 +1,35 @@
 
 public class Aplicacao {
+  private Conteudo[] conteudos = new Conteudo[15];
+  private String[] professores = new String[30];
+  private String[] resumoes = new String[150];
+  private Aluno[] alunos = new Aluno[26000];
+  
+  public Conteudo[] getConteudos() {
+    return conteudos;
+  }
+  public String[] getProfessores() {
+    return professores;
+  }
+  public String[] getResumoes() {
+    return resumoes;
+  }
+  public Aluno[] getAlunos() {
+    return alunos;
+  }
+  public void setConteudos(Conteudo[] conteudos) {
+    this.conteudos = conteudos;
+  }
+  public void setProfessores(String[] professores) {
+    this.professores = professores;
+  }
+  public void setResumoes(String[] resumoes) {
+    this.resumoes = resumoes;
+  }
+  public void setAlunos(Aluno[] alunos) {
+    this.alunos = alunos;
+  }
+
+  
+
 }
